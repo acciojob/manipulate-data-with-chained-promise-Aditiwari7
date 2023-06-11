f91ff5060d710563496946f99c41985dbc083902
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2023-06-11 16:28:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+const arr = [1, 2, 3, 4];
+function promise(arr, time){
+	return new Promise((resolve) =>{
+		setTimeout(() => {
+			resolve(arr);
+		}, time);
+	});
+}
+
+const ans = document.getElementById("output");
+promise(arr,3000).then((res) => {
+	const evenArr = res.filter((num) => {
+		return num%2===0;
+	});
+	return promise(evenArr, 1000);
+}).then((res) => {
+	ans.innerText = res;
+
+	const mulBy2 = res.map((num) => {
+		return num*2;
+	});
+
+	return promise(mulBy2, 2000);
+}).then((res) => {
+	ans.innerText = res;
+});
